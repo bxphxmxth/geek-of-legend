aaf2b41a273fbddcf3c9f34e80f8a5c950c151f9
bxphxmxth <fushs@hotmail.fr>
2021-05-20 09:55:05 +0200

    help

--- src/js/main.js
+++ src/js/main.js
@@ -19,16 +19,28 @@
 
 import {Boss} from "./class.js"
 let boss1 = new Boss("Näz", 2000, 50);
-let boss2 = new Boss("Héritier", 1000, 50);
-let boss3 = new Boss("Nùmy", 800, 40);
-
+let boss2 = new Boss("Héritier", 2000, 50);
+let boss3 = new Boss("Nùmy", 2000, 40);
+console.log(boss1.nom);
 
 // #### A Savoir sur les boss :
 // >* une fois le boss à 20% de ses points de vie, il posera à tes personnages (l'utilisateur) une énigme (choisie aléatoirement entre minimum 3 énigmes) à laquelle ils devront répondre en 3 essais au maximum, s'ils échouent, ils mourront, sinon ils vaincront le boss.
+// import enigme from "./methode.js";
+// enigme();
 
 
-// ​
-// ​
+
+if (this.pv < 400){
+     prompt("qu'est-ce qui est jaune et qui attend? ");
+
+}else{
+     alert("lksjflksdf");
+}
+
+
+
+
+
 // #### Exemples d'énigmes traditionnelles :
 // >* Une fois que l'on me prononce, je n'existe plus. Qui suis-je ?
 // >* Je suis d'eau,je suis d'air,et je suis d'électricité. Qui suis-je ?
@@ -36,7 +48,10 @@ let boss3 = new Boss("Nùmy", 800, 40);
 // >* Quel heure est-il ?(écriture informatisée)
 // >* Quel est l'indice du premier "i" de cette question ?
 // >* Que fait retourne Math.floor(1.3 * 10) ?
-// ​
+
+
+
+
 // >* Le choix du type d'enigme est à votre guise cela peut même être un mix des styles
 // ​
 // ​
---
+++ src/js/methode.js
@@ -0,0 +1,12 @@
+// import { Boss } from "./class"
+
+// let enigme = () =>{
+//      if(Boss.pv <= 20 %){
+//           console.log(gfh);
+//      }else{
+//           console.log(gfh);
+          
+//      }
+// }
+
+// export default enigme;
